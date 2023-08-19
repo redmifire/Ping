fb12455fd38526a5dfa2e298a8bce40481c37a43
redmifire <71125244+redmifire@users.noreply.github.com>
2023-08-19 23:19:19 +0300

    Update weather.py

--- Bots/cogs/weather.py
+++ Bots/cogs/weather.py
@@ -5,7 +5,7 @@ from cogs.weather import command
 class Weathe (commands.cogs):
 @bot.slash_command(name = "Weather", name_localization = "Погода", discription = "Get weather information for a city", description_localization = "Информация о погоде в городе")
 async def weather(ctx, city_name: str, country_code: str, limit: str):
-    api_key = "a1303766d15807177046fa911a6acbb2"
+    api_key = "Your API token"
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&limit={limit}&appid={api_key}"
 
     async with httpx.AsyncClient() as Client:
